0f2a1c78ac351b2455dc76ef5ffb603d9dd335e9
onsummer <onsummer@foxmail.com>
2021-06-08 03:53:20 +0800

    [init] ts project for emit d.ts file only.

---
+++ dist/main.d.ts
@@ -0,0 +1,6 @@
+/**
+ * 一个测试函数
+ * @param {Number[]} center
+ * @param {Number} zoom
+ */
+export function showCenter(center: number[], zoom: number): void;
---
+++ src/main.js
@@ -0,0 +1,12 @@
+/**
+ * 一个测试函数
+ * @param {Number[]} center 
+ * @param {Number} zoom 
+ */
+const showCenter = (center, zoom) => {
+  console.log(`${center}, ${zoom}`)
+}
+
+export {
+  showCenter
+}
